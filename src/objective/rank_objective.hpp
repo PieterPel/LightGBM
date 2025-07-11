e61211a43f892b3245487838b320bf068eeebf40
Pieter Pel <p.pel@robeco.nl>
2025-07-11 12:25:17 +0200

    Updated approximation of listfold

--- src/objective/rank_objective.hpp
+++ src/objective/rank_objective.hpp
@@ -685,9 +685,17 @@ class ApproxListFold : public RankingObjective {
     int n = num_predictions / 2;
     std::vector<double> gradients(num_predictions, 0.0);
     
+    // Linear terms (only added once)
+    for (int j = n; j < 2*n; ++j) {
+      gradients[j] += 1.0;  // top half
+    }
+    for (int j = 0; j < n; ++j) {
+      gradients[j] -= 1.0;  // bottom half
+    }
+    
     for (int i = 0; i < n; ++i) {
       int start = i;
-      int end = num_predictions - i;
+      int end = 2*n + 1 - i;
       
       // Extract segment
       std::vector<double> segment(preds.begin() + start, preds.begin() + end);
@@ -700,29 +708,20 @@ class ApproxListFold : public RankingObjective {
         exp_neg_segment[j] = std::exp(-segment[j]);
       }
       
-      // Compute sums
-      double sum_exp = 0.0;
-      double sum_exp_neg = 0.0;
+      // Compute sums A_i and B_i
+      double A_i = 0.0;
+      double B_i = 0.0;
       for (size_t j = 0; j < exp_segment.size(); ++j) {
-        sum_exp += exp_segment[j];
-        sum_exp_neg += exp_neg_segment[j];
+        A_i += exp_segment[j];
+        B_i += exp_neg_segment[j];
       }
       
-      int correction = end - start;
-      double denom = sum_exp * sum_exp_neg - correction;
+      double C_i = A_i * B_i - (2*n + 1 - 2*i);
       
-      // Add to top half and subtract from bottom half
-      for (int j = n; j < num_predictions; ++j) {
-        gradients[j] += 1.0;
-      }
-      for (int j = 0; j < n; ++j) {
-        gradients[j] -= 1.0;
-      }
-      
-      // Compute and add dlog
+      // Compute and add gradient contribution from log term
       for (int j = 0; j < (end - start); ++j) {
-        double dlog = (exp_segment[j] / sum_exp - exp_neg_segment[j] / sum_exp_neg) / denom;
-        gradients[start + j] += dlog;
+        double grad_contrib = (B_i * exp_segment[j] - A_i * exp_neg_segment[j]) / C_i;
+        gradients[start + j] += grad_contrib;
       }
     }
     
@@ -736,7 +735,7 @@ class ApproxListFold : public RankingObjective {
     
     for (int i = 0; i < n; ++i) {
       int start = i;
-      int end = num_predictions - i;
+      int end = 2*n + 1 - i;
       
       // Extract segment
       std::vector<double> segment(preds.begin() + start, preds.begin() + end);
@@ -749,24 +748,41 @@ class ApproxListFold : public RankingObjective {
         exp_neg_segment[j] = std::exp(-segment[j]);
       }
       
-      // Compute sums
-      double sum_exp = 0.0;
-      double sum_exp_neg = 0.0;
+      // Compute sums A_i and B_i
+      double A_i = 0.0;
+      double B_i = 0.0;
       for (size_t j = 0; j < exp_segment.size(); ++j) {
-        sum_exp += exp_segment[j];
-        sum_exp_neg += exp_neg_segment[j];
+        A_i += exp_segment[j];
+        B_i += exp_neg_segment[j];
       }
       
-      int correction = end - start;
-      double denom = sum_exp * sum_exp_neg - correction;
+      double C_i = A_i * B_i - (2*n + 1 - 2*i);
       
-      // Compute and add d2log
+      // Compute and add diagonal Hessian contribution
       for (int j = 0; j < (end - start); ++j) {
-        double d2log = (
-          (exp_segment[j] * (sum_exp - exp_segment[j])) / (sum_exp * sum_exp) +
-          (exp_neg_segment[j] * (sum_exp_neg - exp_neg_segment[j])) / (sum_exp_neg * sum_exp_neg)
-        ) / denom;
-        hessians[start + j] += d2log;
+        double exp_s_k = exp_segment[j];
+        double exp_neg_s_k = exp_neg_segment[j];
+        
+        // Compute derivatives of A_i and B_i w.r.t. s_k
+        double dA_i_ds_k = exp_s_k;
+        double dB_i_ds_k = -exp_neg_s_k;
+        double d2A_i_ds_k2 = exp_s_k;
+        double d2B_i_ds_k2 = exp_neg_s_k;
+        
+        // Derivatives of C_i w.r.t. s_k
+        double dC_i_ds_k = B_i * dA_i_ds_k + A_i * dB_i_ds_k;
+        double d2C_i_ds_k2 = B_i * d2A_i_ds_k2 + A_i * d2B_i_ds_k2 + 2 * dA_i_ds_k * dB_i_ds_k;
+        
+        // Derivatives of the numerator f_i = B_i * exp(s_k) - A_i * exp(-s_k)
+        double f_i = B_i * exp_s_k - A_i * exp_neg_s_k;
+        double df_i_ds_k = B_i * exp_s_k + A_i * exp_neg_s_k;
+        double d2f_i_ds_k2 = B_i * exp_s_k - A_i * exp_neg_s_k;
+        
+        // Second derivative using quotient rule
+        double numerator = C_i * d2f_i_ds_k2 - f_i * d2C_i_ds_k2;
+        double correction = -2 * (df_i_ds_k * dC_i_ds_k - f_i * dC_i_ds_k) / C_i;
+        
+        hessians[start + j] += numerator / (C_i * C_i) + correction / (C_i * C_i);
       }
     }
     
